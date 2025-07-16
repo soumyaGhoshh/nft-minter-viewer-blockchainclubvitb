f08de07c8f755d0a9c4c12d66412189c5fe77a89
soumyaGhoshh <soumya_ghosh.mail@icloud.com>
2025-07-16 18:42:00 +0530

    commiting the edits

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Buffer } from 'buffer';
+import process from 'process';
+
+// Make globals available for dependencies that need them
+window.Buffer = Buffer;
+window.process = process;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
